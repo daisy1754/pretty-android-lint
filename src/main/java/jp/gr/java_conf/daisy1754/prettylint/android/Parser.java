542ed3269b686b055e386704db4e49c3d521e20d
daisy1754 <d-part@hotmail.co.jp>
2017-09-11 23:51:19 -0700

    Skip warnings

--- src/main/java/jp/gr/java_conf/daisy1754/prettylint/android/Parser.java
+++ src/main/java/jp/gr/java_conf/daisy1754/prettylint/android/Parser.java
@@ -1,6 +1,7 @@
 package jp.gr.java_conf.daisy1754.prettylint.android;
 
 import jp.gr.java_conf.daisy1754.prettylint.android.data.Issue;
+import jp.gr.java_conf.daisy1754.prettylint.android.data.Issue.Severity;
 import jp.gr.java_conf.daisy1754.prettylint.android.data.Location;
 
 import org.w3c.dom.Document;
@@ -41,14 +42,18 @@ public class Parser {
       Node node = nodes.item(i);
       if (node.getNodeType() == Node.ELEMENT_NODE) {
         Element element = (Element) node;
+        Severity severity = Severity.valueOf(element.getAttribute("severity").toUpperCase());
+        if (Severity.WARNING.equals(severity)) {
+          // Ignore warning for now
+          continue;
+        }
         String type = element.getAttribute("id");
-        String severity = element.getAttribute("severity");
         String message = element.getAttribute("message");
         Location location = parseLocation(
             (Element) element.getElementsByTagName("location").item(0));
         // TODO: validate input
         issues.add(
-            new Issue(type, Issue.Severity.valueOf(severity.toUpperCase()), message, location));
+            new Issue(type, severity, message, location));
       }
     }
     return issues;
